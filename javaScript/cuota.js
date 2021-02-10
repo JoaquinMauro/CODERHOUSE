6a3b5b435304b27a308d218bd1dd8b9b5c311493
Joaco <joaquin.gomez89@gmail.com>
2021-02-10 13:31:48 -0300

    cuota

---
+++ javaScript/cuota.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Por favor ingrese su nombre");
+console.log("Hola " + nombre);
+
+let num = prompt("Ingrese un número");
+let suma = parseInt(num) + 10;
+console.log(suma);
+
+let text1 = prompt("Ingrese su nombre");
+let text2 = prompt("Ingrese su apellido");
+console.log(text1 + " " + text2);
+
+
